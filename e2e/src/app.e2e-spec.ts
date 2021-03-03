92a240fdd5aa9cc8ca2c6036ba5f309c095aa17e
gojnimer <gojnimer@gmail.com>
2021-03-02 22:22:24 -0300

    update

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('notes-conversor app is running!');
+    expect(await page.getTitleText()).toEqual('pokemon-project app is running!');
   });
 
   afterEach(async () => {
--- karma.conf.js
+++ karma.conf.js
@@ -19,7 +19,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/notes-conversor'),
+      dir: require('path').join(__dirname, './coverage/pokemon-project'),
       subdir: '.',
       reporters: [
         { type: 'html' },
